e807b6dbfb2eead916744313809fe0d5ed7c3cfa
Alex Bondrea <sabondrea1@sheffield.ac.uk>
2019-05-17 21:40:50 +0100

    Fixed my events not loading

--- routes/myevents.js
+++ routes/myevents.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 router.get('/all', (req, res) => myEventController.all(res));
 
-router.get('/:myEventId', (req, res) => myEventController.getMyEvent(req, res));
-
 router.get('/new', function (req, res) {
     res.render('myevent/new');
 });
 
 router.post('/new', (req, res) => myEventController.new(req,res));
+
+router.get('/:myEventId', (req, res) => myEventController.getMyEvent(req, res));
 
 module.exports = router;
